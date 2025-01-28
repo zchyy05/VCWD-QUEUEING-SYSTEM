0f481352d4eb7091f592f159b7f174cfbc94ed4d
zchyy05 <nealterence731@gmail.com>
2025-01-28 16:51:30 +0800

    Add client folder and its contents

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="justify-center">
+        <h1 className="text-center">Hello World</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
